5694560426b1469e19868ddecd62764dfbd1db4b
nanamatsumoto <71700324+nanamatsumoto@users.noreply.github.com>
2020-11-01 17:40:28 +0900

    新規投稿商品ボタンのリンク

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :title, :text, :category_id, :condition_id, :postage_id, :price, :region_id, :shipping_date_id).merge(user_id: current_user.id)
