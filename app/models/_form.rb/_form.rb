23b184f6e19b25ba51bef97f908e2bddfaa2a4bb
nanamatsumoto <71700324+nanamatsumoto@users.noreply.github.com>
2020-11-02 18:39:57 +0900

    バリデーションの変更

--- app/models/_form.rb/_form.rb
+++ app/models/_form.rb/_form.rb
@@ -17,7 +17,7 @@ class UserDonation
   belongs_to :purchase
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase:purchase, user_id: user.id)
     # 各テーブルにデータを保存する処理を書く
   end
