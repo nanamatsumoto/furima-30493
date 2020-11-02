a3d1a912fa18f74c7be18385475c2cbd15eb8e0e
nanamatsumoto <71700324+nanamatsumoto@users.noreply.github.com>
2020-11-02 14:51:37 +0900

    レビュー修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user_id
+      @item.destroy
+    end  
+      redirect_to root_path
   end
 
   def update
