7cd30ec4da19566d2fe6b4b46b820ac4ea4320a2
nanamatsumoto <71700324+nanamatsumoto@users.noreply.github.com>
2020-11-03 15:00:56 +0900

    不要な記載の削除

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,7 +2,6 @@ class Form
   include ActiveModel::Model
   attr_accessor :post_code,:prefectures_id, :city, :house_number, :building_name, :phone_number, :purchase
 
-  # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :post_code
     validates :prefectures_id
@@ -14,11 +13,8 @@ class Form
     validates :purchase
   end
 
-    # belongs_to :purchase
-
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase:purchase, user_id: user.id)
-    # 各テーブルにデータを保存する処理を書く
   end
 end
