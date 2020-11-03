ca488c8d432122209595462714ee74e4e1e7e554
nanamatsumoto <71700324+nanamatsumoto@users.noreply.github.com>
2020-11-03 15:00:11 +0900

    購入画面への遷移

--- app/controllers/purchase_controller.rb
+++
@@ -1,9 +0,0 @@
-class PurchaseController < ApplicationController
-
-  def index
-    @form = Form.new
-  end
-
-  def create
-  end
-end
---
+++ app/models/form.rb
@@ -0,0 +1,24 @@
+class Form
+  include ActiveModel::Model
+  attr_accessor :post_code,:prefectures_id, :city, :house_number, :building_name, :phone_number, :purchase
+
+  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :post_code
+    validates :prefectures_id
+    validates :city
+    validates :house_number
+    validates :building_name
+    validates :phone_number
+    validates :user_id
+    validates :purchase
+  end
+
+    # belongs_to :purchase
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase:purchase, user_id: user.id)
+    # 各テーブルにデータを保存する処理を書く
+  end
+end
