6a97b3c9c7c3757ff64b4513a7d8c615d4d3a142
nanamatsumoto <71700324+nanamatsumoto@users.noreply.github.com>
2020-11-04 12:13:59 +0900

    コントローラー修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,22 @@
 class PurchasesController < ApplicationController
-  before_action :set_form, only: [:create]
-
   def index
+    # @form = Item.form.includes(:item)
     @form = Form.new
   end
 
   def create
-    # binding.pry
+    # @form = Item.form.includes(:item)
+    @form = Form.new(form_params)
+    if @form.save
+      return redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
-  def set_form
-    @form = Form.find(params[:id])
-   end
+  private
+
+  def form_params
+    params.require(:form).permit(:post_code, :prefectures_id, :city, :house_number, :building_name, :phone_number, :purchase, :user_id, :item_id)
+  end
 end
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,6 +1,6 @@
 class Form
   include ActiveModel::Model
-  attr_accessor :post_code,:prefectures_id, :city, :house_number, :building_name, :phone_number, :purchase
+  attr_accessor :post_code,:prefectures_id, :city, :house_number, :building_name, :phone_number, :purchase, :user_id, :item_id
 
   with_options presence: true do
     validates :post_code
@@ -15,6 +15,6 @@ class Form
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase:purchase, user_id: user.id)
+    Address.create(post_code: post_code, prefectures_id: prefectures_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
   end
 end
