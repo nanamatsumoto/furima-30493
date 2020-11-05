82438fc38f4ceebbc62d7a5dd551384c014027f1
nanamatsumoto <71700324+nanamatsumoto@users.noreply.github.com>
2020-11-05 21:13:13 +0900

    テストコード修正

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :form do
     
-    post_code {123-4567}
+    post_code {"123-4567"}
     prefectures_id {2}
     city {"aあア"}
     house_number {"aあア"}
     building_name {"aあア"}
-    phone_number {11111111111}
-    user_id {1}
-    item_id {1}
-    purchase {1}
+    phone_number {"11111111111"}
     token {"tok_abcdefghijk00000000000000000"}
+
+    association :user
+    association :item
   end
 end
--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -1,47 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe Form, type: :model do
+  describe Form do
   before do
-    @form = FactoryBot.build(:form)
+    @item = FactoryBot.build(:item)
+    @user = FactoryBot.build(:user)
+    @form = FactoryBot.build(:form, item_id: @item.id, user_id: @user.id)
   end
 
-  it "post_codeが空では保存ができないこと" do
-    @form.post_code = ""
-    @form.valid?
-    expect(@form.errors.full_messages).to include("Post code can't be blank")
-  end
+    it "post_codeが空では保存ができないこと" do
+      @form.post_code = ""
+      @form.valid?
+      expect(@form.errors.full_messages).to include("Post code can't be blank")
+    end
 
-  it "cityが空では保存ができないこと" do
-    @form.city = ""
-    @form.valid?
-    expect(@form.errors.full_messages).to include("City can't be blank")
-  end
+    it "post_codeがハイフンなしでは保存ができないこと" do
+      @form.post_code = "1234567"
+      @form.valid?
+      binding.pry
+      expect(@form.errors.full_messages).to include("Post code is invalid")
+    end
 
-  it "house_numberが空では保存ができないこと" do
-    @form.house_number = ""
-    @form.valid?
-    expect(@form.errors.full_messages).to include("House number can't be blank")
-  end
+    it "prefectures_idが空では保存ができないこと" do
+      @form.prefectures_id = ""
+      @form.valid?
+      expect(@form.errors.full_messages).to include("Prefectures can't be blank")
+    end
 
-  it "phone_numberが空では保存ができないこと" do
-    @form.phone_number = ""
-    @form.valid?
-    expect(@form.errors.full_messages).to include("Phone number can't be blank")
-  end
+    it "cityが空では保存ができないこと" do
+      @form.city = ""
+      @form.valid?
+      expect(@form.errors.full_messages).to include("City can't be blank")
+    end
 
-  it "id番号は1では保存ができないこと" do
-    @form.prefectures_id = 1
-    @form.valid?
-    expect(@form.errors.full_messages).to include("Prefectures must be other than 1")
-  end
+    it "house_numberが空では保存ができないこと" do
+      @form.house_number = ""
+      @form.valid?
+      expect(@form.errors.full_messages).to include("House number can't be blank")
+    end
 
-  it "tokenが空では登録できないこと" do
-    @form.token = nil
-    @form.valid?
-    expect(@form.errors.full_messages).to include("Token can't be blank")
-  end
+    it "phone_numberが空では保存ができないこと" do
+      @form.phone_number = ""
+      @form.valid?
+      expect(@form.errors.full_messages).to include("Phone number can't be blank")
+    end
 
-  it "全てが入力されていたら保存ができること" do
-    expect(@form).to be_valid
+    it "phone_numberが数字のみかつ11桁でないと保存ができないこと" do
+      @form.phone_number = "1111111111"
+      @form.valid?
+      expect(@form.errors.full_messages).to include("Phone number is invalid")
+    end
+
+    it "prefectures_id番号は1では保存ができないこと" do
+      @form.prefectures_id = 1
+      @form.valid?
+      expect(@form.errors.full_messages).to include("Prefectures must be other than 1")
+    end
+
+    it "prefectures_id番号は空では保存ができないこと" do
+      @form.prefectures_id = ""
+      @form.valid?
+      expect(@form.errors.full_messages).to include("Prefectures can't be blank")
+    end
+
+    it "tokenが空では登録できないこと" do
+      @form.token = nil
+      @form.valid?
+      expect(@form.errors.full_messages).to include("Token can't be blank")
+    end
+
+    it "全てが入力されていたら保存ができること" do
+      expect(@form).to be_valid
+    end
   end
 end
